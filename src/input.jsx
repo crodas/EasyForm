789c5fe00ed326568490e844ae7ec183d4456a1b
Cesar Rodas <cesar@rodas.me>
2016-08-01 18:53:12 -0400

    Added dropzone support
    It wraps "dropzone" library for browser uplodas

--- src/input.jsx
+++ src/input.jsx
@@ -4,6 +4,7 @@ import TextArea from './inputs/textarea.jsx';
 import DropDown from './inputs/select.jsx';
 import ArrayContainer from './container/array.jsx';
 import Container from './container/container.jsx';
+import Dropzone from './inputs/dropzone.jsx';
 
 import './validate.jsx'
 
@@ -23,4 +24,5 @@ export {
     TextArea,
     DropDown,
     DropDown as Select,
+    Dropzone,
 };
